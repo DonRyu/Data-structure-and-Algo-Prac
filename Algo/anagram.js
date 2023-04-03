04acffc71a339e501db420efda388da37f17af01
DonRyu <alpharyu1031@gmail.com>
2023-04-03 13:10:45 -0700

    anagram done

--- Algo/anagram.js
+++ Algo/anagram.js
@@ -1,3 +1,4 @@
+// error version
 const validAnagram = (word, word2) => {
   let set = new Set();
   let validWord = word.toLowerCase().replace(/\s+/g, "");
@@ -16,4 +17,37 @@ const validAnagram = (word, word2) => {
   console.log(set.size === set2.size ? "true" : "false");
 };
 
-validAnagram("someeee", "eeesome"); // false
+// anagram
+// gramana
+
+const validAnagram2 = (word, word2) => {
+  let letter = {};
+
+  if(word.length !== word2.length){
+    return false
+  }
+
+  for (let i = 0; i < word.length; i++) {
+    let item = word[i]
+
+    if (letter[item]) {
+      letter[item] += 1;
+    } else {
+      letter[item] = 1;
+    }
+  }
+
+  for (let i = 0; i < word2.length; i++) {
+    let item = word2[i]
+
+    if (!letter[item]) {
+      return false;
+    } else {
+      letter[item] -= 1;
+    }
+  }
+
+  return true
+};
+
+validAnagram2("asdasd", "dsadsads");
