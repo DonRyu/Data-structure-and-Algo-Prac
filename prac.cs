4829365a0b074d57642187f44c3b48647967fa63
DonRyu <alpharyu1031@gmail.com>
2023-04-25 13:05:45 -0700

    paldinrome

---
+++ prac.cs
@@ -0,0 +1,14 @@
+// Palindrome Creator  
+// ‘madjsam’ => “madam” (연속적으로 최대 두개 char만 지울수 있음)
+// “Asd” => not possible
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("asd");
+    }
+    
+}
