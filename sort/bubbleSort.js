0d3ff654f705f0197e01de49cf74c17409e2ed75
DonRyu <alpharyu1031@gmail.com>
2023-04-28 12:15:15 -0700

    edit

--- sort/bubbleSort.js
+++ sort/bubbleSort.js
@@ -1,16 +1,21 @@
 
 
 const bubbleSort = (arr) => {
+  let noSwap; // Optimization
   for (let i = arr.length; i > 0; i--) {
+    noSwap = true;
     for (let j = 0; j < i-1; j++) {
       if (arr[j] > arr[j + 1]) {
         let tmp = arr[j];
         arr[j] = arr[j+1];
         arr[j+1] = tmp;
+        noSwap = false;
       }
     }
+    if(noSwap) break;
+
   }
   return arr;
 };
 
-console.log(bubbleSort([8, 5, 3]));
+console.log(bubbleSort([1,3,2,8]));
