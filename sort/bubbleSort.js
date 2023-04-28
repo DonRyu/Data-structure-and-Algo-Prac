ba6118437304cacee18bba2bec59d72c35ce95b2
DonRyu <alpharyu1031@gmail.com>
2023-04-27 17:30:22 -0700

    bubble sort

---
+++ sort/bubbleSort.js
@@ -0,0 +1,14 @@
+const bubbleSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let tmp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = tmp;
+      }
+    }
+  }
+  return arr;
+};
+
+console.log(bubbleSort([8, 5, 3, 6, 2, 1]));
