2b375cf602fed0844ca0d6a99d44cba1ffc5b931
DonRyu <alpharyu1031@gmail.com>
2023-05-01 12:03:32 -0700

    summary

--- sort/insertionSort.js
+++ sort/insertionSort.js
@@ -1,20 +1,21 @@
 
-
-
+// O(n^2)
 function insertionSort(arr) {
   let currentVal;
-  // i = 1
+  
+  //i = 1
   for (let i = 1; i < arr.length; i++) {
     currentVal = arr[i]; // 5
     let j;
-    // j = 1 && 8 > 5
+    // j = 0 , 0>=0 && 8>5, 
     for (j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
-      arr[j + 1] = arr[j]; // arr[2] = arr[1]
+      arr[j + 1] = arr[j]; // arr[1] = arr[0] / 5 = 8 / 5,8,3
+      console.log(`=>${j} ${arr}`)
     }
-     arr[j + 1] = currentVal;
+     arr[j + 1] = currentVal; // arr[0] = 5 
   }
   return arr;
 }
 
-console.log(insertionSort([8, 5, 3, 2]));
+console.log(insertionSort([8, 5]));
 // 5,8,3,2
