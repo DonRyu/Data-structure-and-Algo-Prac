743eb6b60bce17356c9ce880f20b25fb40f305c9
don <donryu1031@gmail.com>
2023-08-20 20:05:54 -0700

    contain dupli

--- InterviewPrep/containDuplicate.js
+++ InterviewPrep/containDuplicate.js
@@ -1,4 +1,5 @@
-var containsDuplicate = function(nums) {
+//o(n)
+const containsDuplicate = function(nums) {
     let set = new Set();
 
     for(let i = 0; i<nums.length; i++){
@@ -11,3 +12,53 @@ var containsDuplicate = function(nums) {
     }
     return false;
 };
+
+const containsDuplicate2 = nums => new Set(nums).size !== nums.length;
+
+const containsDuplicate3 = () => {
+    let map = {};
+    
+}
+
+//sorted nlogn
+const containsDuplicate4 = (nums)=>{
+    let sorted = nums.sort((a,b)=>a-b);
+
+    for (let i = 0; i < nums.length; i++) {       
+        if(sorted[i] === sorted[i+1]) return true
+    }
+
+    return false
+}
+
+
+
+
+// set
+
+// 1 1 => 1
+// set(arr).size === arr.length
+
+// 123 123 false
+// 12  123 true 
+
+
+const containDuplicate = arr => new Set(arr).size !== arr.length;
+
+// 123
+// 1 true
+// 1 1
+// 3 2
+
+const containDuplicate2 = (arr)=>{
+    let set = {};
+
+    for (let i = 0; i < arr.length; i++) {
+        if(set[arr[i]]){
+            return true;
+        }
+        set[arr[i]] = true;    
+    }
+
+    return false;
+}
