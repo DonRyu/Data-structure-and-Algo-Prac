ed778ab85a15f7b736020cf6976cd1136c98604f
don <donryu1031@gmail.com>
2023-09-04 13:34:58 -0700

    edit

--- InterviewPrep/maxSubArr.js
+++ InterviewPrep/maxSubArr.js
@@ -1,16 +1,18 @@
 var maxSubArray = function(nums) {
-    let max;
+    let max = nums[0];
+    let currSum = 0;
+  
+    for(let i =0; i<nums.length; i++){
+      currSum += nums[i]
 
-    for(let i = 0; i<nums.length; i++){
-        let nMax;
-        let subArr = nums.slice(i);
-        subArr.map((item)=>{
-            nMax += item
-        })
-        max = Math.max(max,nMax)
+      if(currSum < nums[i]){
+        currSum = nums[i]
+      }
+
+      max = Math.max(currSum,max)
+
     }
-
-    return max;
+    return max
 };
 
-maxSubArray([])
+console.log(maxSubArray([-2,1,-3]))
