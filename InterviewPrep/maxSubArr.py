5316b0bace40a5fb40b09a3e82cc4b03e021fd55
don <donryu1031@gmail.com>
2023-09-05 15:25:27 -0700

    add

--- InterviewPrep/maxSubArr.py
+++ InterviewPrep/maxSubArr.py
@@ -1,3 +1,10 @@
 def maxSubArr(nums):
     sub_max = nums[0]
-    return  sub_max  
+    curr_max = 0
+    for i in nums:
+            curr_max += i
+            if curr_max < i:
+               curr_max = i
+            sub_max = max(sub_max,curr_max)
+        
+    return sub_max
