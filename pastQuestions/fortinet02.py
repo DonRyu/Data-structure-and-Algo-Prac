e13986bef51795f990e4b0bc702773c8a520b8fb
don <donryu1031@gmail.com>
2023-09-12 18:01:48 -0700

    edit

--- pastQuestions/fortinet02.py
+++ pastQuestions/fortinet02.py
@@ -1,8 +1,8 @@
 grid = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
 
 def findSafeWays(grid):
-    n = len(grid) #3
-    m = len(grid[0]) #3
+    n = len(grid) 
+    m = len(grid[0])
     mod = (10**9) + 7
 
     dp = [[0] * m for _ in range(n)] 
@@ -14,12 +14,8 @@ def findSafeWays(grid):
             if grid[i][j] == 1:
                 if i > 0:
                     dp[i][j] += dp[i-1][j]
-                    # print("i",dp[i][j])
                 if j > 0: 
                     dp[i][j] += dp[i][j-1]
-                    # print("j",dp[i][j])
-                dp[i][j]
-                
     return dp[n-1][m-1] % mod
 
 # Example usage
--- pracP.py
+++ pracP.py
@@ -2,11 +2,30 @@
 
 grid = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
 
-n = len(grid)
-m = len(grid[0])
+# grid[n-1][m-1] = grid[n-1][m] + grid[n][m-1]
+# 
+# 1 1 1
+# 1 0 1
+# 1 1 1
 
-dp = [[0]* m for _ in range(n)]
+grid = [[1,1,1],[1,0,1],[1,1,1]]
+def findTheWay(grid):
+    n = len(grid)
+    m = len(grid[0])
+    mod = (10**9)+7
 
-test = [[1]*5]
+    dp = [[0]* m for _ in range(n)]
+    dp[0][0] = 1
 
-print(dp)
+    for i in range(n):
+        for j in range(m):
+            if grid[i][j] == 1:
+                if i>0:
+                    dp[i][j] += dp[i-1][j]
+                if j>0:
+                    dp[i][j] += dp[i][j-1]    
+    
+    return dp[n-1][m-1] % mod
+
+
+print(findTheWay(grid))
