dd15c052bf1bb4fdafe1faa19cd50113cde79b75
don <donryu1031@gmail.com>
2023-09-15 16:25:22 -0700

    closestSum

--- pracP.py
+++ pracP.py
@@ -1,31 +1,21 @@
+def closestSum(nums,target):
+    l,r = 0, len(nums)-1
+    closest_sum = float('inf')
 
+    for i in range(len(nums)):
+        l = i+1
 
-grid = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+        while l <= r:
+                curr_sum = nums[i]+nums[l]+nums[r]
 
-# grid[n-1][m-1] = grid[n-1][m] + grid[n][m-1]
-# 
-# 1 1 1
-# 1 0 1
-# 1 1 1
+                if(closest_sum>abs(target-curr_sum)):
+                    closest_sum = abs(target-curr_sum)
 
-grid = [[1,1,1],[1,0,1],[1,1,1]]
-def findTheWay(grid):
-    n = len(grid)
-    m = len(grid[0])
-    mod = (10**9)+7
-
-    dp = [[0]* m for _ in range(n)]
-    dp[0][0] = 1
-
-    for i in range(n):
-        for j in range(m):
-            if grid[i][j] == 1:
-                if i>0:
-                    dp[i][j] += dp[i-1][j]
-                if j>0:
-                    dp[i][j] += dp[i][j-1]    
-    
-    return dp[n-1][m-1] % mod
-
-
-print(findTheWay(grid))
+                if curr_sum < target:
+                    l +=1
+                if  curr_sum > target:  
+                    r -=1
+                if  curr_sum == target:
+                    return curr_sum    
+                
+    return closest_sum
