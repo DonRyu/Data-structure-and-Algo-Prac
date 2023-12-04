5a853df8187f57de459278a9d213c23bf3ad8dd9
don <donryu1031@gmail.com>
2023-12-04 15:25:10 -0800

    add q

--- pastQuestions/Saije.js
+++ pastQuestions/Saije.js
@@ -57,7 +57,7 @@ const getReduce = (nData) => {
       acc[address] = { key, date };
     }
     return acc;
-  },{});
+  }, {});
 
   return result;
 };
@@ -67,16 +67,108 @@ console.log(getReduce(nData));
 // ------------------------------------------------------------------------------------------------
 
 // Email Threads: Given a list of email threads with unique identifiers,
-// subject lines, and timestamps, write a script to find the email with the latest timestamp for each unique subject line.
+// subject lines, and timestamps, write a script to find
+// the email with the latest timestamp for each unique subject line.
 
-// Social Media Posts: Process a set of social media posts with unique identifiers,
-// user handles, text content, and posting dates. Identify the post with the most comments and the post with the least comments for each unique user handle.
+let emails = [
+  {
+    key: 0,
+    subject: "so what",
+    timeStamp: 2234,
+  },
+  {
+    key: 2,
+    subject: "so what",
+    timeStamp: 2934,
+  },
+  {
+    key: 5,
+    subject: "so what3",
+    timeStamp: 3934,
+  },
+  {
+    key: 9,
+    subject: "so what3",
+    timeStamp: 5934,
+  },
+];
 
-// File Revisions: Given a dataset of file revisions with unique identifiers,
-// filenames, authors, and modification dates, write a function to find the most recent revision and the least recent revision for each unique filename.
+const getLatestTimeStamp = (emails) => {
+  let result = emails.reduce((acc, curr) => {
+    let { key, subject, timeStamp } = curr;
 
-// Restaurant Menus: Process a list of restaurant menu items with unique identifiers,
-//dish names, prices, and last update dates. Determine the menu item with the highest price and the menu item with the lowest price for each unique dish name.
+    if (!acc[subject] || acc[subject].timeStamp < timeStamp) {
+      acc[subject] = { key, timeStamp };
+    }
+    return acc;
+  }, {});
+  return result;
+};
 
-// Website Visits: Given a dataset of website visits with unique identifiers,
-//user IDs, page URLs, and visit timestamps, write a script to find the most recent visit and the least recent visit for each unique user ID.
+console.log(getLatestTimeStamp(emails))
+
+
+
+// 문제 1: 배열 평균 계산
+
+// 주어진 JSON 데이터에서 숫자로 이루어진 배열을 찾아 해당 배열의 평균값을 계산하세요.
+
+
+
+// const jsonData = {
+//   "numbers": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// };
+
+
+
+
+
+
+// 문제 2: 짝수만 필터링
+
+//     주어진 JSON 데이터에서 짝수로 이루어진 배열을 찾아 출력하세요.
+
+
+
+// const jsonData = {
+//   "numbers": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// };
+
+// 문제 3: 이름 길이 계산
+
+//     주어진 JSON 데이터에서 이름을 담은 배열을 찾아, 각 이름의 길이를 계산하여 새로운 배열로 출력하세요.
+
+
+
+// const jsonData = {
+//   "names": ["Alice", "Bob", "Charlie", "David", "Eve"]
+// };
+
+// 문제 4: 과일 가격 총합 계산
+
+//     주어진 JSON 데이터에서 과일과 가격으로 이루어진 배열을 찾아, 모든 과일의 가격을 더한 값을 계산하세요.
+
+
+
+// const jsonData = {
+//   "fruits": [
+//     {"name": "Apple", "price": 2},
+//     {"name": "Banana", "price": 1},
+//     {"name": "Orange", "price": 3},
+//     {"name": "Grapes", "price": 4}
+//   ]
+// };
+
+// 문제 5: 단어 빈도수 계산
+
+//     주어진 JSON 데이터에서 문장을 담은 배열을 찾아, 모든 단어의 빈도수를 계산하여 객체로 출력하세요.
+
+
+
+// const jsonData = {
+//   "sentences": [
+//     "This is a sample sentence.",
+//     "Another sentence for practice.",
+//     "Sample sentence for the exercise."
+//   ]
+// };
