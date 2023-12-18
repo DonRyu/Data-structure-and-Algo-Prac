e5f6deb913d8dd9b0dd0dda256293e11e8bce541
don <donryu1031@gmail.com>
2023-12-18 11:09:19 -0800

    prac

--- pastQuestions/minisis.js
+++ pastQuestions/minisis.js
@@ -5,9 +5,7 @@
 // database 가 머냐?
 // 그냥 text file과 database의 차이가 머냐?
 
-
 // toUppercase가 머냐고 인터뷰어 한테 물었는데, 사장이 인터뷰 보는 사람 누구냐고 이야기함, 내가 잘못한건가?
-
 
 // part 2
 // 1a.) Obesverations: We have a string an initial string and need to have it changed.
@@ -44,8 +42,6 @@ const ChangeString = (str) => {
     }
   }
 
-
-
   keyArr = Object.keys(map); // [a,b,c,d]
   valueArr = Object.values(map); // [1,2,3]
 
@@ -61,12 +57,34 @@ const ChangeString = (str) => {
   return answer.toUpperCase();
 };
 
-// console.log(ChangeString(str));
-
-
-
-
-
-
 //--------------------------------------------------------------------------------------------
 
+//abcAEBCCfg => A2B2C3E1F1G1
+
+const changeString2 = (str) => {
+  let upperStr = str.toUpperCase();
+  let arr = upperStr.split("");
+  let keyArr = [];
+  let valueArr = [];
+  let result = "";
+
+  let map = arr.reduce((acc, curr) => {
+    if (acc[curr]) {
+      acc[curr] += 1;
+    } else {
+      acc[curr] = 1;
+    }
+    return acc;
+  }, {});
+
+  keyArr = Object.keys(map);
+  valueArr = Object.values(map);
+
+  for (let i = 0; i < keyArr.length; i++) {
+    result += `${valueArr[i]}${keyArr[i]}`;
+  }
+
+  return result;
+};
+
+console.log(changeString2("abcAEBCCfg"));
